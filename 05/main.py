17d2b2bd6b255c1ffcf60010a772ae29e07cd81b
iammunna-devmushfiqrmunna@gmail.com <you@example.com>
2025-11-25 11:38:43 +0600

    Reorganize: moved files into 05 folder

--- 05/main.py
+++ 05/main.py
@@ -1,7 +1,46 @@
-##FOR LOOPS
+'''
+#For loop
+exam_scores=[80,90,95,83,77,72,85,93,97]
+total=0
+for scores in exam_scores:
+    total+=scores
+print(total)
 
-players=["Liton","Tanzid","Emon","Mushfiq","Sabbir","Nasir","Shakib"]
-for player in players:
-    print(player)
-    print(players)
-print(players)
+#Finding sum without for loop
+
+exam_scores=[80,90,95,83,77,72,85,93,97]
+print(sum(exam_scores))
+
+
+
+#Finding max using for loop
+exam_scores=[80,90,95,83,100,72,85,93,97]
+result=0
+for maximum in exam_scores:
+    if maximum>result:
+        result=maximum
+print(result)
+
+#finding max without loop
+exam_scores=[80,90,95,83,100,72,85,93,97]
+print(max(exam_scores))
+
+
+##Range function (1,10) does not include 10 and in default it increase by 1 but we can add custom increase number by (1,10,3) now it will increase by 3
+
+#Sum of numbers in a range using for loops
+total=0
+for sum_of_numbers in range(1,101):
+    total+=sum_of_numbers
+print(total)
+
+#Sum of numbers in a range without using loops
+print(sum(range(1,101)))
+
+##While Loop
+num=10
+while num>0:
+    print(f"i love you {num}")
+    num-=1
+
+    '''
--- 05/project.py
+++ 05/project.py
@@ -1,1 +1,31 @@
-print("the length is: "+ len(input("Enter Your name: ")))
+
+'''
+#Task 1
+for numbers in range(1,11):
+    print(numbers)
+
+
+#Task 2
+for numbers in range(2,51,2):
+    print(numbers)
+
+
+#Task 3
+number=10
+while number>0:
+    print(number)
+    number-=1
+
+#Task 4
+total=0
+for sum in range(1,11):
+    total+=sum
+print(total)
+
+
+##Task 5
+value=int(input("Enter a number:\n"))
+for number in range(1,11):
+    print(f"{value} x {number} = {value*number}")
+    
+'''
