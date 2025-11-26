0d3d814e244fb2e5be9013bc286de9b13d03b986
iammunna-devmushfiqrmunna@gmail.com <you@example.com>
2025-11-26 10:20:34 +0600

    update

---
+++ pass_generator.py
@@ -0,0 +1,23 @@
+##RANDOM PASSWORD GENERATOR
+
+import random
+
+letter=["A", "a", "B", "b", "c", "C", "D", "d", "E", "e", "F", "f", "G", "g", "H" , "h", "I", "i", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n"]
+number=[0,1,2,3,4,5,6,7,8,9]
+symbol=["!","@","#","$","%","&","*","(",")","-","+","/","?","~"]
+
+user_number=int(input("How many number in your password you want?"))
+user_letter=int(input("How many letter in your password you want?"))
+user_symbol=int(input("How many symbol in your password you want?"))
+
+rand_number=(random.choices(number, k=user_number))
+rand_letter=(random.choices(letter,k=user_letter))
+rand_symbol=(random.choices(symbol,k=user_symbol))
+
+password=rand_letter+rand_number+rand_symbol
+random.shuffle(password)
+password="".join(str(x) for x in password)
+print(password)
+
+
+
